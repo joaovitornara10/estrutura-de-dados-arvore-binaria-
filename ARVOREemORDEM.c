148349f51bcb1bd0e9560acd5b0588a7e0944205
joaovitornara10 <54191082+joaovitornara10@users.noreply.github.com>
2019-08-16 20:54:26 -0400

    Add files via upload

---
+++ ARVOREemORDEM.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+struct noArvore {
+	int info;
+	struct noArvore* esq;
+	struct noArvore* dir;
+};
+
+typedef struct noArvore no;
+
+no* criar_no(int info, no* esq, no* dir){
+	
+	no* n = malloc(sizeof(no));
+	
+	n->info = info;
+	n->esq  = esq;
+	n->dir  = dir;
+	
+	return n;
+}	 
+
+void impArvore(no* n){
+	if(n != NULL){
+
+	impArvore(n->esq);
+	printf("(%d)", n->info);
+	impArvore(n->dir);
+
+	}
+}
+
+void main(){
+	no* nono = 
+	criar_no(1, criar_no(2, criar_no(3, NULL, NULL),criar_no(4, NULL, NULL)),
+				criar_no(5, criar_no(6, NULL, NULL),criar_no(7, NULL, NULL)));
+	impArvore(nono);
+}
+
+
+
+
